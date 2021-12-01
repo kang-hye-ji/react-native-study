73ebcc2c818c1762961826e49c6658224419af4d
kanghyeji <kanghyeji@hyeji-MacBookPro.local>
2021-12-01 14:25:57 +0900

    refactor: App 리팩토링
    - loading 상태에 따라 AppLoading 또는 LoggedOutNav 를 렌더할 수 있도록 수정

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import AppLoading from 'expo-app-loading';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import LoggedOutNav from './navigators/LoggedOutNav';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map(font => Font.loadAsync(font));
+    const imagesToLoad = [require('./assets/instagram_logo.png')];
+    console.log(fontPromises);
+    const imagePromises = imagesToLoad.map(image => Asset.loadAsync(image));
+
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+
+  if (loading) {
+    return (
+      <AppLoading
+        onError={console.warn}
+        onFinish={() => setLoading(!loading)}
+        startAsync={preload}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
