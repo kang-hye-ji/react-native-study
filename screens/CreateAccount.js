ceb8294e5095bfd3999bc0afb41d98ef1444c73d
kanghyeji <kanghyeji@hyeji-MacBookPro.local>
2021-12-02 14:34:24 +0900

    refactor: CreateAccount 수정
    - TextInput의 다양한 기능을 알아보기 위해 TextInput 을 배치
    - 키보드에서 submit 버튼을 클릭 시 다음 TextInput 으로 포커스가 이동하는 로직 적용

--- screens/CreateAccount.js
+++ screens/CreateAccount.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { KeyboardAvoidingView } from 'native-base';
+import React, { useRef } from 'react';
+import { TextInput } from 'react-native';
+import AuthButton from '../components/auth/AuthButton';
+import AuthLayout from '../components/auth/AuthLayout';
 
 export default function CreateAccount() {
+  const lastNameRef = useRef();
+  const onFirstNameNext = () => {
+    lastNameRef?.current?.focus();
+  };
   return (
-    <View>
-      <Text>CreateAccount</Text>
-    </View>
+    <AuthLayout>
+      <KeyboardAvoidingView behavior="height">
+        <TextInput
+          onSubmitEditing={onFirstNameNext}
+          placeholder="First Name"
+          returnKeyType="go"
+        ></TextInput>
+        <TextInput
+          placeholder="Last Name"
+          ref={lastNameRef}
+          returnKeyType="go"
+        ></TextInput>
+        <TextInput placeholder="User Name" returnKeyType="go"></TextInput>
+        <TextInput placeholder="Email" returnKeyType="go"></TextInput>
+        <TextInput
+          secureTextEntry
+          placeholder="Password"
+          keyboardType="password"
+          returnKeyType="go"
+        ></TextInput>
+        <AuthButton
+          onPress={() => null}
+          disabled="true"
+          text="Create Account"
+        ></AuthButton>
+      </KeyboardAvoidingView>
+    </AuthLayout>
   );
 }
