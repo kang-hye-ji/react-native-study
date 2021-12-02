16f0b186892b4d1e25609d7e1d613630594602bf
kanghyeji <kanghyeji@hyeji-MacBookPro.local>
2021-12-02 14:28:20 +0900

    refactor: LoggedOutNav 의 각 Stack.Screen 컴포넌트에 필요한 options properties 추가

--- navigators/LoggedOutNav.js
+++ navigators/LoggedOutNav.js
@@ -9,9 +9,21 @@ const Stack = createStackNavigator();
 export default function LoggedOutNav() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen
+        name="Welcome"
+        options={{ headerShown: false }}
+        component={Welcome}
+      />
       <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="CreateAccount" component={CreateAccount} />
+      <Stack.Screen
+        name="CreateAccount"
+        component={CreateAccount}
+        options={{
+          headerShown: false,
+          headerTransparent: true,
+          headerTintColor: 'white',
+        }}
+      />
     </Stack.Navigator>
   );
 }
