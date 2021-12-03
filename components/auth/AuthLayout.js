ec45949572f93fff775466279ad25655f94b39c6
강혜지 <kanghyeji@hyeji-MacBookPro.local>
2021-12-03 19:16:28 +0900

    refactor: AuthLayout 리팩토링 화면을 터치하면 키보드가 사라질 수 있는 로직 구현   - 전체를 Touchable 컴포넌트로 랩핑하여 터치할 수 있는 구역으로 지정   - Keyboard API 를 활용하여 키보드가 사라지는 로직 구현
    KeyboardAvoidingView 를 공통적으로 사용하므로 AuthLayout  내부로 이관

--- components/auth/AuthLayout.js
+++ components/auth/AuthLayout.js
@@ -1,17 +1,33 @@
-import { Box, Flex, Image } from 'native-base';
+import { Flex, Image } from 'native-base';
 import React from 'react';
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 export default function AuthLayout({ children }) {
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
-    <Flex alignItems="center" justifyContent="center" flex="1">
-      <Image
-        source={require('../../assets/instagram_logo.png')}
-        resizeMode="contain"
-        maxW="50%"
-        h="24"
-        mb="5"
-      ></Image>
-      {children}
-    </Flex>
+    <TouchableWithoutFeedback
+      style={{ display: 'flex', flex: 1 }}
+      onPress={dismissKeyboard}
+    >
+      <Flex alignItems="center" justifyContent="center" flex="1">
+        <KeyboardAvoidingView behavior="position">
+          <Image
+            source={require('../../assets/instagram_logo.png')}
+            resizeMode="contain"
+            maxW="50%"
+            h="24"
+            mb="5"
+          ></Image>
+          {children}
+        </KeyboardAvoidingView>
+      </Flex>
+    </TouchableWithoutFeedback>
   );
 }
