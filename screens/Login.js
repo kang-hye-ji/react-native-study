25a07a3179a8e3ce7dfae64d27efda5ca1f3a2d4
강혜지 <kanghyeji@hyeji-MacBookPro.local>
2021-12-03 19:24:20 +0900

    refactor: Login - 로그인 버튼 클릭 시 AuthAPI 를 호출하는 로직 구현

--- screens/Login.js
+++ screens/Login.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { useRef, useEffect } from 'react';
+import AuthLayout from '../components/auth/AuthLayout';
+import { Input } from 'native-base';
+import { useForm } from 'react-hook-form';
+import AuthButton from '../components/auth/AuthButton';
+import { AuthAPI } from '../api/auth';
+import { logIn } from '../auth/logIn';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Login({ navigation }) {
+  const { register, handleSubmit, setValue } = useForm();
+  const passwordRef = useRef();
+  const onNext = next => {
+    next?.current?.focus();
+  };
+  useEffect(() => {
+    register('username');
+    register('password');
+  }, [register]);
+  const onValid = async data => {
+    const response = await AuthAPI.getToken({
+      email: 'hello@a-ha.io',
+      password: 'Thecorp!40519',
+    });
+    await logIn(response.data.token);
+    const test = await AsyncStorage.getItem('token');
+    console.log(test);
+  };
   return (
-    <View>
-      <Text>Login</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
-        <Text>Go to Create Account</Text>
-      </TouchableOpacity>
-    </View>
+    <AuthLayout>
+      <Input
+        placeholder="Username"
+        returnKeyType="next"
+        autoCapitalize="none"
+        onSubmitEditing={onNext(passwordRef)}
+        onChangeText={text => setValue('username', text)}
+      ></Input>
+      <Input
+        ref={passwordRef}
+        placeholder="Password"
+        secureTextEntry
+        returnKeyType="done"
+        onSubmitEditing={onValid}
+        onChangeText={text => setValue('password', text)}
+      ></Input>
+      <AuthButton text="Log in" onPress={handleSubmit(onValid)}></AuthButton>
+    </AuthLayout>
   );
 }
